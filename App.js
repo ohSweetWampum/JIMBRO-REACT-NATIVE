9c8bb85cf3c25bed9837aeb88f29b3555f3a8981
Matt <mtgibson888@gmail.com>
2023-07-13 12:14:48 -0500

    finally got the newuserlanding page to show up on mobile screen

--- App.js
+++ App.js
@@ -1,58 +1,47 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Header } from 'react-native-elements';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {enableScreens} from 'react-native-screens';
+enableScreens();
 
-import NewUserLandingPage from '../screens/newUserLandingPage';
-import CreateAccount from '../screens/createAccount';
-import EnterBasicBodyInfo from '../screens/enterBasicBodyInfo';
+import NewUserLandingPage from './client/screens/newUserLandingPage';
+import CreateAccount from './client/screens/createAccount';
+import EnterBasicBodyInfo from './client/screens/enterBasicBodyInfo';
 
 const Stack = createStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#007aff', // Set your primary color here
+    accent: '#ff3d00', // Set your accent color here
+  },
+};
 
 function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="NewUserLandingPage">
-          <Stack.Screen 
-            name="NewUserLandingPage" 
-            component={NewUserLandingPage} 
-            options={{ header: () => 
-              <Header
-                placement="left"
-                leftComponent={{ icon: 'menu', color: '#fff' }}
-                centerComponent={{ text: 'Home', style: { color: '#fff' } }}
-                rightComponent={{ icon: 'home', color: '#fff' }}
-              /> 
-            }}
-          />
-          <Stack.Screen 
-            name="CreateAccount" 
-            component={CreateAccount} 
-            options={{ header: () => 
-              <Header
-                placement="left"
-                leftComponent={{ icon: 'arrow-back', color: '#fff' }}
-                centerComponent={{ text: 'Create Account', style: { color: '#fff' } }}
-                rightComponent={{ icon: 'home', color: '#fff' }}
-              /> 
-            }}
-          />
-          <Stack.Screen 
-            name="EnterBasicBodyInfo" 
-            component={EnterBasicBodyInfo} 
-            options={{ header: () => 
-              <Header
-                placement="left"
-                leftComponent={{ icon: 'arrow-back', color: '#fff' }}
-                centerComponent={{ text: 'Enter Body Info', style: { color: '#fff' } }}
-                rightComponent={{ icon: 'home', color: '#fff' }}
-              /> 
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <PaperProvider theme={theme}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="NewUserLandingPage">
+            <Stack.Screen 
+              name="NewUserLandingPage" 
+              component={NewUserLandingPage} 
+            />
+            <Stack.Screen 
+              name="CreateAccount" 
+              component={CreateAccount} 
+            />
+            <Stack.Screen 
+              name="EnterBasicBodyInfo" 
+              component={EnterBasicBodyInfo} 
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
     </SafeAreaProvider>
   );
 }
